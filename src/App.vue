51cc4726fd253633ef9e2964202c95aaca4db62b
nomikazu <>
2021-06-26 08:27:04 +0900

    add is

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
   <div>
-    <LikeHeader v-slot="slotProps">
-      <p>{{ slotProps }}</p>
-      <h2>みなさん</h2>
+    <LikeHeader>
       <h3>はじめまして</h3>
-      <p>よろしく</p>
     </LikeHeader>
     <LikeNumber :total-number='number' @my-click='incrementNumber'></LikeNumber>
-    <LikeNumber :total-number='number'></LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <component :is="currentComponent"></component>
   </div>
 </template>
 
 <script>
 import LikeHeader from './components/LikeHeader.vue';
+import About from './components/About.vue';
+import Home from './components/Home.vue';
+
 
 export default {
   data() {
     return { 
-      number: 10
+      number: 10,
+      currentComponent: 'Home'
     }
   },
   components: {
-    LikeHeader
+    LikeHeader,
+    About,
+    Home
   },
   methods: {
     incrementNumber(value) {
