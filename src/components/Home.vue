d19e1f5dfc4c513c7c00db4003a8f50cba5826f4
nomikazu <>
2021-06-26 12:04:31 +0900

    add modifiers

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <p v-border:dotted="{width: '5px', color: 'red'}">Home</p>
+    <p v-border:solid.round.shadow="{width: '5px', color: 'red'}">Home</p>
   </div>
 </template>
