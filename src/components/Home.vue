e7bc16ca8dd9cf27db25fb40d85eb08add7d39e2
nomikazu <>
2021-06-26 13:55:15 +0900

    add filter

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <p v-border:solid.round.shadow="{width: '5px', color: 'red'}">Home</p>
+    <h2>{{ title | upperCase }}</h2>
+    <p>{{ subTitle | upperCase }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      tmpData: "hello",
+      title: 'Welcome to Tokyo',
+      subTitle: 'Tokyo is a greate city'
+    };
+  },
+  
   directives: {
     border(el, binding) {
       el.style.borderWidth = binding.value.width;
