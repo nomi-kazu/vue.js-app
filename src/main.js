3b36e285efba963abd9acc09e6dbcad0424ed8f4
nomikazu <>
2021-06-26 11:49:06 +0900

    add v-border

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import LikeNumber from './components/LikeNumber.vue'
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
+Vue.directive("border", function(el, binding) {
+
+});
 
 new Vue({
   render: h => h(App),
