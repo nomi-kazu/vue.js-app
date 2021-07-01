2915b1619e7cddf9ae5063c16bd63baa35ff92dc
nomikazu <>
2021-07-01 21:55:40 +0900

    add router-link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <nav>
+      <router-link
+        to="/" 
+        active-class="link--active"
+        exact
+        class="link"
+      >Home</router-link>
+      <router-link
+        to="/users"
+        active-class="link--active"
+        class="link"
+      >Users</router-link>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -12,5 +25,11 @@ export default {
 </script>
 
 <style scoped>
+.link {
+  margin-right: 10px;
+}
 
+.link--active {
+  font-size: 20px;
+}
 </style>
