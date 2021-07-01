aed264f385155da01ebccc0e6b0e35b48997a103
nomikazu <>
2021-07-01 22:45:54 +0900

    add users.id

--- src/router.js
+++ src/router.js
@@ -7,5 +7,6 @@ import Users from "./views/Users.vue";
 Vue.use(Router);
 
 export default new Router({
-  routes: [{ path: "/", component: Home }, { path: "/users", component: Users }]
+  mode: "history",
+  routes: [{ path: "/", component: Home }, { path: "/users/:id", component: Users }]
 })
